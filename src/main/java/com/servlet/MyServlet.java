c51bd0b1c926a205378dec8b2b1b2cabc43e0812
zhangbingxiao <2282032693@qq.com>
2020-04-21 18:29:34 +0800

    servlet1.0版本

---
+++ src/main/java/com/servlet/MyServlet.java
@@ -0,0 +1,26 @@
+package com.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author zhangbingxiao
+ * @Date 2020-04-21 18:16
+ * @Version 1.0
+ */
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("bingxiao");
+    }
+}
